9469734f20660e931e273639c3615cfa596da71a
kyunghyukcho <rudgur1996@hanmail.net>
2022-07-06 16:10:50 +0900

    2022.07.06

--- Argorithm_BaekJoon/Dynamic Programming/2240.py
+++ Argorithm_BaekJoon/Dynamic Programming/2240.py
@@ -5,7 +5,6 @@ plum = [0]
 
 for i in range(t):
     plum.append(int(input()))
-print(plum)
 for i in range(w+1):
     for j in range(1, t+1):
         if i%2==0:
@@ -18,4 +17,17 @@ for i in range(w+1):
                 dp[j] = max(dp[j], dp[j-1])
             elif plum[j]==2:
                 dp[j] = max(dp[j]+1, dp[j-1]+1)
-    print(dp)
+print(dp[-1])
+    
+'''
+every index of zero initialize 'zero'
+PLUM  2 1 1 2 2 1 1 
+DP    0 0 0 0 0 0 0
+i=0 (w=0) 일 때 --> 현재 Tree의 위치는 1번 
+DP    0 1 2 2 2 3 4 
+i=1 (w=1) 일 때 --> 현재 Tree의 위치는 2번 
+DP    1 1 2 3 4 4 4 
+i=2 (w=2) 일 때 --> 현재 Tree의 위치는 1번 
+DP    1 2 2 3 4 5 6
+
+'''
