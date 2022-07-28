5935bea8a39437e2d107539e9d9aa8cd5f124e82
kyunghyukcho <rudgur1996@hanmail.net>
2022-07-29 01:18:25 +0900

    2022.07.29

--- Argorithm_BaekJoon/Dynamic Programming/13398_연속합2.py
+++ Argorithm_BaekJoon/Dynamic Programming/13398_연속합2.py
@@ -26,16 +26,16 @@
 
 
 
-N = int(input())
+n = int(input())
 arr = list(map(int, input().split()))
-dp = [[0] * N for _ in range(2)]
+dp = [[0] * n for _ in range(2)]
 dp[0][0] = arr[0] 
-if N > 1:
-    ans = -1e9
-    for i in range(1, N):
+if n > 1:
+    answer = -1000000
+    for i in range(1, n):
         dp[0][i] = max(dp[0][i - 1] + arr[i], arr[i])
         dp[1][i] = max(dp[0][i - 1], dp[1][i-1] + arr[i])
-        ans = max(ans, dp[0][i], dp[1][i])
-    print(ans)
+        answer = max(answer, dp[0][i], dp[1][i])
+    print(answer)
 else:
     print(dp[0][0])
