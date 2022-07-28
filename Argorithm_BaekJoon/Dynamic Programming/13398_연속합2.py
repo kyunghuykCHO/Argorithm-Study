d02161d736a67b79268abe80dafddde9570f1c77
kyunghyukcho <rudgur1996@hanmail.net>
2022-07-29 00:09:49 +0900

    2022.07.29

---
+++ Argorithm_BaekJoon/Dynamic Programming/13398_연속합2.py
@@ -0,0 +1,41 @@
+# n = int(input())
+# arr = list(map(int,input().split()))
+# dp = [-1000] * len(arr)
+# dp[0] = arr[0]
+# maxValue = -1000
+
+# for i in range(1,len(arr)):
+#     for j in range(1,len(arr)):
+#         if i==j:
+#             continue
+#         elif j==i+1:
+#             dp[j] = max(arr[j],dp[j-2]+arr[j])
+#             maxValue = max(maxValue,dp[j])
+#         else:
+#             dp[j] = max(arr[j],dp[j-1]+arr[j])
+#             maxValue = max(maxValue,dp[j])
+#     dp = [-1000] * len(arr)
+#     dp[0] = arr[0]    
+# print(max(maxValue,sum(arr)))
+
+
+
+
+# ===================================================================================
+
+
+
+
+N = int(input())
+arr = list(map(int, input().split()))
+dp = [[0] * N for _ in range(2)]
+dp[0][0] = arr[0] 
+if N > 1:
+    ans = -1e9
+    for i in range(1, N):
+        dp[0][i] = max(dp[0][i - 1] + arr[i], arr[i])
+        dp[1][i] = max(dp[0][i - 1], dp[1][i-1] + arr[i])
+        ans = max(ans, dp[0][i], dp[1][i])
+    print(ans)
+else:
+    print(dp[0][0])
